77a4932792dd9efdbb6beb1ae8ef2bda115fac1b
Keyversly <keyversly@host.voo.be>
2025-11-20 03:18:48 -0600

    Create responsive header navigation, Add Desktop menu with link mapping, Implement active router highlighting using usePathname, Add Hover effects with animated underline, Make menu responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
   return (
-    <header className="bg-white py-5">
-      <Container className="">
+    <header className="bg-white py-5 border-b border-b-black/50">
+      <Container className="flex items-center justify-between">
         {/* Logo */}
         <Logo />
         {/* NavButton */}
+        <HeaderMenu />
+        <div>Others</div>
         {/* NavAdmin */}
       </Container>
     </header>
