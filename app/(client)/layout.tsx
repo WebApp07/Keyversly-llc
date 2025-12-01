ef5ee2d2692bcb04bfc514b0986cdbc7fc1ae082
Keyversly <keyversly@host.voo.be>
2025-12-01 17:26:23 +0100

    Setup sanity localy

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -18,15 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
+      <body>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </ClerkProvider>
   );
 }
