9f3ec16b2e6719e775e046de87fc094b8d53ea0c
Keyversly <keyversly@host.voo.be>
2025-12-04 01:12:11 +0100

    Header sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = async () => {
   console.log(user, "user");
 
   return (
-    <header className="bg-white py-5">
+    <header className="bg-white/70 py-5 sticky top-0 z-50 backdrop-blur-md">
       <Container className="flex items-center justify-between text-lightColor">
         {/* Logo */}
         <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
