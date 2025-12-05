b091930fb290fde2eb4d2b236bc8c94f1e21fb0f
Keyversly <keyversly@host.voo.be>
2025-12-05 23:38:21 +0100

    Starting homeCategories

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
+import HomeCategories from "@/components/HomeCategories";
 import ProductGrid from "@/components/ProductGrid";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <HomeBanner />
       <div className="py-10">
         <ProductGrid />
+        <HomeCategories />
       </div>
     </Container>
   );
---
+++ components/HomeCategories.tsx
@@ -0,0 +1,5 @@
+const HomeCategories = () => {
+  return <div>HomeCategories</div>;
+};
+
+export default HomeCategories;
