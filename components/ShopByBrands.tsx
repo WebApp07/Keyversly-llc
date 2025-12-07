5c193b29b56811542508ef0588b4439bb2631840
Keyversly <keyversly@host.voo.be>
2025-12-07 01:41:24 +0100

    add features section

--- components/ShopByBrands.tsx
+++ components/ShopByBrands.tsx
@@ -3,6 +3,30 @@ import Title from "./Title";
 import { getAllBrands } from "@/sanity/queries";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
+import { GitCompareArrows, Headset, ShieldCheck, Truck } from "lucide-react";
+
+const extraData = [
+  {
+    title: "Free Delivery",
+    description: "Free shipping over $100",
+    icon: <Truck size={45} />,
+  },
+  {
+    title: "Free Return",
+    description: "Free shipping over $100",
+    icon: <GitCompareArrows size={45} />,
+  },
+  {
+    title: "Customer Support",
+    description: "Friendly 27/7 customer support",
+    icon: <Headset size={45} />,
+  },
+  {
+    title: "Money Back guarantee",
+    description: "Quality checked by our team",
+    icon: <ShieldCheck size={45} />,
+  },
+];
 
 // This is an async server component.
 // It runs on the server, so it can fetch data directly from Sanity.
@@ -48,6 +72,24 @@ const ShopByBrands = async () => {
               />
             )}
           </Link>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-16 p-2 shadow-sm hover:shadow-shop_light_green/20 py-5">
+        {extraData?.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-3 group text-lightColor hover:text-shop_light_green"
+          >
+            <span className="inline-flex scale-100 group-hover:scale-90 hoverEffect">
+              {item?.icon}
+            </span>
+            <div className="text-sm">
+              <p className="text-darkColor/80 font-bold capitalize">
+                {item?.title}
+              </p>
+              <p className="text-lightColor">{item?.description}</p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
