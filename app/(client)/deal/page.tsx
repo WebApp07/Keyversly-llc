3b5166417891def3c9c74598c6ffe20f7e8cc60f
Keyversly <keyversly@host.voo.be>
2025-12-08 23:27:13 +0100

    DealPage: Fetch and display all hot deal products from Sanity

--- app/(client)/deal/page.tsx
+++ app/(client)/deal/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Container from "@/components/Container";
+import ProductCard from "@/components/ProductCard";
+import Title from "@/components/Title";
+import { getDealProducts } from "@/sanity/queries";
 
-const DealPage = () => {
-  return <div>DealPage</div>;
+const DealPage = async () => {
+  const products = await getDealProducts();
+  return (
+    <div className="py-10 bg-shop_light_bg">
+      <Container>
+        <Title className="mb-5 underline underline-offset-4 decoration-1 text-base uppercase tracking-wide">
+          Hot Deals of the Week
+        </Title>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+          {products?.map((product) => (
+            <ProductCard key={product?._id} product={product} />
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default DealPage;
