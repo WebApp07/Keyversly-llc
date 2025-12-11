694d4a9ded6f74ffd5183f6b6917bfff3c71806d
Keyversly <keyversly@host.voo.be>
2025-12-11 11:45:23 +0100

    Add link for productCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -13,16 +13,17 @@ const ProductCard = ({ product }: { product: Product }) => {
     <div className="text-sm border rounded-md border-darkBlue/20 group bg-white">
       <div className="relative group overflow-hidden bg-shop_light_bg">
         {product?.images && (
-          <Image
-            src={urlFor(product?.images[0]).url()}
-            alt="ProductImage"
-            loading="lazy"
-            width={700}
-            height={700}
-            //priority
-            className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg duration-500 
+          <Link href={`/product/${product?.slug?.current}`}>
+            <Image
+              src={urlFor(product.images[0]).url()}
+              alt="productImage"
+              width={500}
+              height={500}
+              priority
+              className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg duration-500 
               ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
-          />
+            />
+          </Link>
         )}
 
         <AddToWishListButton product={product} />
