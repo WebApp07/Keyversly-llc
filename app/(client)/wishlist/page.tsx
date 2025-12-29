3465d5a402bff0be55543e33fb4ba375c1af5667
WebApp07 <aminezouguig@proton.me>
2025-12-29 15:20:28 +0100

    build page wishlist

--- app/(client)/wishlist/page.tsx
+++ app/(client)/wishlist/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import NoAccess from "@/components/NoAccess";
+import WishListProducts from "@/components/WishListProducts";
+import { currentUser } from "@clerk/nextjs/server";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const WishListPage = async() => {
+    const user = await currentUser()
+ return (
+    <>
+        {user? <WishListProducts /> : <NoAccess details="Log in to view your wishlist items. Don’t miss out on your cart products to make the payment!" />}
+    </>
+ )
 }
 
-export default page
+export default WishListPage;
