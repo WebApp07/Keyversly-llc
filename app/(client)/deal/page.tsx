10ed66c004b92ae6484e6c0263e51941dc78ae8f
WebApp07 <aminezouguig@proton.me>
2026-01-06 23:57:29 +0100

    fixed typescript deal page

--- app/(client)/deal/page.tsx
+++ app/(client)/deal/page.tsx
@@ -6,13 +6,13 @@ import { getDealProducts } from "@/sanity/queries";
 const DealPage = async () => {
   const products = await getDealProducts();
   return (
-    <div className="py-10 bg-shop_light_bg">
+    <div className="py-10 bg-deal-bg">
       <Container>
-        <Title className="mb-5 underline underline-offset-4 decoration-1 text-base uppercase tracking-wide">
+        <Title className="mb-5 underline underline-offset-4 decoration-[1px] text-base uppercase tracking-wide">
           Hot Deals of the Week
         </Title>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-          {products?.map((product) => (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5">
+          {products?.map((product: typeof products[number]) => (
             <ProductCard key={product?._id} product={product} />
           ))}
         </div>
